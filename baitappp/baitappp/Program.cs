07fb671b7b17c0d7e87cd93e3b8596561dbeb2fc
thang <thangtu0440@gmail.com>
2025-03-17 21:13:58 +0700

    baitap

---
+++ baitappp/baitappp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baitappp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tong = 0;
+            int[] mang = new int[5];
+            mang[0]= 4;
+            mang[1]= 14;
+            mang[2]= 2;
+            mang[3]= 6;
+            mang[4]= 1;
+
+            for (int i = 0; i < mang.Length; i++) 
+            { 
+                Console.WriteLine(mang[i]);
+            }
+            for (int i = 0; i < mang.Length; i++) 
+            {
+                
+                 tong = tong + mang[i];
+               
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("Tong:"+tong);
+        }
+
+    }
+}
---
+++ baitappp/baitappp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("baitappp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("baitappp")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac6289d6-2276-495c-936e-23933e4b82ea")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
